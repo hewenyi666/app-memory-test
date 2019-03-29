efc4454f18133220c53ea28cf8ddac49b6541c0e
hewenyi666 <47819184+hewenyi666@users.noreply.github.com>
2019-03-29 14:04:49 +0800

    Update 新建note.py

--- test files/新建note.py
+++ test files/新建note.py
@@ -1,22 +1,22 @@
-#新建note 10次
+#新建笔记 10次
 from uiautomator import Device
 import os
 import time
-d=Device('04110258C5000088')
-os.popen('adb shell am start -n com.hanvon.xpen/com.hanvon.xboard.activities.HistoryAty')
+d=Device('序列号')#通过adb devices获取
+os.popen('activity')#activity替换为要测试界面的activity
 time.sleep(1)
-d(resourceId='com.hanvon.xpen:id/img_note').click()
+d(resourceId='id名称').click()#id名称通过uiautomatorviewer获取
 time.sleep(0.5)
 with open(r'../data/start-note.txt','w') as file2:
     file2.write('11')
 time1=time.time()
 for i in range(10):
      print('正在进行第%d次测试' % (i+1))
-     d(resourceId='com.hanvon.xpen:id/text_create').click()
+     d(resourceId='id名称').click()
      time.sleep(1)
-     d(resourceId='com.hanvon.xpen:id/ai_lansel_ok_tv').click()#点击确定
+     d(resourceId='id名称').click()#点击确定
      time.sleep(1)
-     d(resourceId='com.hanvon.xpen:id/note_close').click()
+     d(resourceId='id名称').click()
 time2=time.time()
 worktime=time2-time1
 with open(r'../data/stop-note.txt','w',encoding='utf-8') as fff:
